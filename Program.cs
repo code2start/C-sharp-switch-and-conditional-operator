27996eb19e33e6c8e1ba3ce77d15b54871bd166e
Mohamed Fathy <codetwostart@gmail.com>
2019-10-12 13:56:27 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace switchandifoperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int guess = 33;
+            switch (guess)
+            {
+                case 3:
+                case 1:
+                    Console.WriteLine("Three");
+                    Console.WriteLine("One");
+                    break;
+                case 2:
+                    Console.WriteLine("Two");
+                    break;
+                default:
+                    Console.WriteLine("Invalid");
+                    break;
+            } */
+           /*  bool agree = false;
+            string result = (agree) ? "the result is OK" : "not good";
+            Console.WriteLine(result); */
+            int i = 0;
+            while(i < 5){
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
